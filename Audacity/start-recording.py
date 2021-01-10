36e5ff88f280d5910c419cc27da32bdae2aa8a5d
Kevin Colby <kevincolby@outlook.com>
2021-01-10 12:17:10 -0600

    initial commit

---
+++ Audacity/start-recording.py
@@ -0,0 +1,74 @@
+import os, time, datetime, logging, json, sys
+import pipeclient
+
+# create logger
+logger = logging.getLogger('start-recording')
+logger.setLevel(logging.DEBUG)
+
+logDirectory = os.path.join(os.path.dirname(os.path.realpath (__file__)), 'logs')
+timeNow = datetime.datetime.now()
+logFileName = timeNow.strftime("%Y%m%d") + 'pipeclient.log'
+logPath = os.path.join( logDirectory, logFileName)
+
+# create file handler which logs even debug messages
+fh = logging.FileHandler(logPath)
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+logger.info('Starting Pipeclient')
+client = pipeclient.PipeClient()
+
+logger.info('Getting Current Tracks')
+client.write('getInfo: Type=Tracks')
+while client.reply == '':
+    time.sleep(0.1)
+
+pipeJSON = client.read()
+if ('' == pipeJSON): 
+    logger.error('**********No response from Audactiy**********')
+    # TODO:do something here to flag the issue
+    sys.exit()
+pipeJSON = pipeJSON[:pipeJSON.rfind('BatchCommand finished: OK')]
+pipeResponse = json.loads(pipeJSON)
+
+logger.info("JSON Response: " + pipeJSON)
+
+if (len(pipeResponse) == 0):
+    logger.info("No tracks found. Clear to proceed")
+    # Start Recording on New Track
+else:
+    logger.info("Found " + str(len(pipeResponse)) + " existing track(s)")
+    logger.info("removing existing tracks")
+    # TODO: Check if currently recording, stop if so.
+    # logger.debug("Pause")
+    # client.write('Pause:')
+    # while client.reply == '':
+    #     time.sleep(0.1)
+    logger.debug("SelAllTracks")
+    client.write('SelAllTracks:')
+    while client.reply == '':
+        time.sleep(0.1)
+    logger.debug("RemoveTracks")
+    client.write('RemoveTracks:')
+    while client.reply == '':
+        time.sleep(0.1)
+
+# TODO: Set correct audio device
+logger.info("Starting new Recording")
+client.write('Record2ndChoice:')
+while client.reply == '':
+    time.sleep(0.1)
+
+
+
+
+
